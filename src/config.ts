8e6c5b2b0642124d146d056a8c128968fbe39399
yuhuixx <claraxu1201@gmail.com>
2024-09-02 23:43:34 +0200

    no port

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-let apiUrl = 'https://wb-i-xtech.azurewebsites.net:8001';
+let apiUrl = 'https://wb-i-xtech.azurewebsites.net';
 
 // if (import.meta.env.VITE_REACT_APP_NODE_ENV === 'development')
 //     apiUrl = import.meta.env.VITE_REACT_APP_LOCAL_API_URL as string;
